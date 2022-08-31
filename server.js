653f565fc834299b1f9f2124ff03c382d9d43dfc
kalai <kalaichandran369@gmail.com>
2022-08-31 11:05:37 +0400

    commit changes

--- server.js
+++ server.js
@@ -12,12 +12,7 @@ dotenv.config();
 
 const app = express();
 
-const corsOptions = {
-  origin: "http://localhost:3000",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
